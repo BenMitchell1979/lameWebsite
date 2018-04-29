9c95eb3a282cfeeebd26d6f050817b7219d2df04
Benjamin <benmitchell1979@outlook.com>
2018-04-28 21:47:55 -0400

    update dsc

--- dsc/deployWebsite.ps1
+++ dsc/deployWebsite.ps1
@@ -1,8 +1,31 @@
-configure deployWebsite {
+Configuration deployWebsite {
     import-dscresource -modulename 'PSDesiredStateConfiguration'
 
+    # Install WebServer
     windowsfeature WebServer {
         Ensure = 'Present'
         Name = 'Web-Server'
+    }
+
+    # Download file
+    File downloadWebsite {
+        Ensure = 'Present'
+        DestinationPath = 'c:\inetpub\website.zip'
+        SourcePath = 'https://github.com/BenMitchell1979/lameWebsite/raw/master/data/website.zip'
+        Type = 'File'
+    }
+
+    Script extractWebsite {
+        SetScript = {
+            Expand-Archive -Force -Path "c:\inetpub\website.zip" -DestinationPath "C:\inetpub\wwwroot\";
+        }
+
+        TestScript = {
+            (test-path -Path "c:\inetpub\wwwroot");
+        }
+
+        GetScript = {
+            # Nothing to see here
+        }
     }
 }
